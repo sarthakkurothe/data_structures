5aa3edeb601172e674d71e1e61b5bca101b12d2e
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-02-23 14:46:12 +0530

    dp and graph implementation

---
+++ graph/cheapestflight.cpp
@@ -0,0 +1,75 @@
+/* 787. Cheapest Flights Within K Stops
+
+https://leetcode.com/problems/cheapest-flights-within-k-stops/?envType=daily-question&envId=2024-02-23
+
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+
+
+
+Example 1:
+
+
+Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+Output: 700
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
+Example 2:
+
+
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+Example 3:
+
+
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+Output: 500
+Explanation:
+The graph is shown above.
+The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+
+*/
+class Solution
+{
+public:
+    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int K)
+    {
+        unordered_map<int, vector<pair<int, int>>> adjList;
+        for (auto f : flights)
+            adjList[f[0]].push_back({f[1], f[2]});
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+        minHeap.push({0, src, 0});
+
+        vector<int> dist(n + 1, INT_MAX);
+
+        while (!minHeap.empty())
+        {
+            auto t = minHeap.top();
+            minHeap.pop();
+            int cost = t[0];
+            int curr = t[1];
+            int stop = t[2];
+
+            if (curr == dst)
+                return cost;
+
+            if (dist[curr] < stop)
+                continue;
+            dist[curr] = stop;
+
+            if (stop > K)
+                continue;
+
+            for (auto next : adjList[curr])
+                minHeap.push({cost + next.second, next.first, stop + 1});
+        }
+        return -1;
+    }
+};

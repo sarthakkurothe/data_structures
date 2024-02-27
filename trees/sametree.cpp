daec71d72fe415fcb96aca4cd295068915f640a3
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-02-27 18:59:26 +0530

    bit manipulation and trees

---
+++ trees/sametree.cpp
@@ -0,0 +1,58 @@
+/* 100. Same Tree
+
+https://leetcode.com/problems/same-tree/description/?envType=daily-question&envId=2024-02-26
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+
+Example 1:
+
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+*/
+
+class Solution
+{
+public:
+    bool solve(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL && q == NULL)
+        {
+            return true;
+        }
+        if (p == NULL && q != NULL)
+        {
+            return false;
+        }
+        if (p != NULL && q == NULL)
+        {
+            return false;
+        }
+        if (p->val != q->val)
+        {
+            return false;
+        }
+        bool a = solve(p->left, q->left);
+        bool b = solve(p->right, q->right);
+        return a && b;
+    }
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        return solve(p, q);
+    }
+};

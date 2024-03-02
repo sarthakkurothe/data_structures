aebd56ea0ecbad592d60fccc824304e22afb85f2
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-03-02 18:38:24 +0530

    some arrays stuff

---
+++ arrays/squaresortedarrays.py
@@ -0,0 +1,26 @@
+# 977. Squares of a Sorted Array
+
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+
+
+#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+#Example 1:
+#Input: nums = [-4,-1,0,3,10]
+#Output: [0,1,9,16,100]
+#Explanation: After squaring, the array becomes [16,1,0,9,100].
+#After sorting, it becomes [0,1,9,16,100].
+
+
+#Example 2:
+#Input: nums = [-7,-3,2,3,11]
+#Output: [4,9,9,49,121]
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        nums_squared = [num ** 2 for num in nums]
+        nums_squared.sort()
+        return nums_squared
+ 
